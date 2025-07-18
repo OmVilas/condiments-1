489aa31cf231cc9c7a7e27ec54ccf151667fc495
Niroshkumar10 <niroshkumar35@gmail.com>
2025-07-18 13:21:03 +0530

    new file added

--- client/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div className="container mt-4">
-      <h2>Our Story</h2>
-      <p>From humble beginnings in Coimbatore, we’ve brought traditional snack flavors to thousands of homes across India...</p>
-    </div>
-  );
-};
-export default About;
--- client/src/pages/Admin.jsx
+++
@@ -1,9 +0,0 @@
-const Admin = () => {
-  return (
-    <div className="container mt-4">
-      <h2>Admin Panel</h2>
-      <p>Manage product uploads, orders, users (To be built).</p>
-    </div>
-  );
-};
-export default Admin;
